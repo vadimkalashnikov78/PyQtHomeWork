c5e98b27bb2cf003ce79fb6bfc3096adddb8dc87
vadimkalashnikov78 <128225806+vadimkalashnikov78@users.noreply.github.com>
2023-12-14 18:01:08 +0300

    HW 3 формы

--- HW3/a_threads.py
+++ HW3/a_threads.py
@@ -6,24 +6,25 @@ import time
 
 import psutil
 from PySide6 import QtCore
+from PySide6.QtCore import Signal
 
 
 class SystemInfo(QtCore.QThread):
-    systemInfoReceived = ...  # TODO Создайте экземпляр класса Signal и передайте ему в конструктор тип данных передаваемого значения (в текущем случае list)
+    systemInfoReceived = Signal(list)  # TODO Создайте экземпляр класса Signal и передайте ему в конструктор тип данных передаваемого значения (в текущем случае list)
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        ...  # TODO создайте атрибут класса self.delay = None, для управлением задержкой получения данных
+        self.delay = None
 
-    def run(self) -> None:  # TODO переопределить метод run
-        if self.delay is None:  # TODO Если задержка не передана в поток перед его запуском
-            self.delay = ...  # TODO то устанавливайте значение 1
+    def run(self) -> None:
+        if self.delay is None:
+            self.delay = 1
 
         while True:  # TODO Запустите бесконечный цикл получения информации о системе
-            cpu_value = psutil  # TODO с помощью вызова функции cpu_percent() в пакете psutil получите загрузку CPU
-            ram_value = ...  # TODO с помощью вызова функции virtual_memory().percent в пакете psutil получите загрузку RAM
-            self.systemSignal  # TODO с помощью метода .emit передайте в виде списка данные о загрузке CPU и RAM
-            time  # TODO с помощью функции .sleep() приостановите выполнение цикла на время self.delay
+            cpu_value = psutil.cpu_percent(1, False)
+            ram_value = psutil.virtual_memory().percent
+            self.__class__.systemInfoReceived.emit(cpu_value, ram_value)  # TODO с помощью метода .emit передайте в виде списка данные о загрузке CPU и RAM
+            time.sleep(self.delay)
 
 
 class WeatherHandler(QtCore.QThread):
--- HW3/b_systeminfo_widget.py
+++ HW3/b_systeminfo_widget.py
@@ -11,3 +11,50 @@
 5. установку времени задержки сделать "горячей", т.е. поток должен сразу
 реагировать на изменение времени задержки
 """
+import psutil
+from PySide6 import QtWidgets
+from form1 import Ui_Form
+from a_threads import SystemInfo
+
+
+class SystemInfoForm(QtWidgets.QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setupUi(self)
+        self.initSignals()
+
+
+    def initSignals(self):
+        """
+
+        :return:
+        """
+        self.lineEdit.textChanged.connect(self.onTextChanged)
+        self.progress()
+
+
+
+
+    def onTextChanged(self):
+        """
+
+        :return:
+        """
+
+    def progress(self):
+        """
+
+        :return:
+        """
+            self.lcdNumber.display(psutil.cpu_percent(1, False))
+            self.lcdNumber_2.display(psutil.virtual_memory().percent)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = SystemInfoForm()
+    window.show()
+
+    app.exec()
